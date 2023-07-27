456246224d9e48d944bd0c73f061d2d57bd601c0
shadowmoon0721 <shadowmoon0721@gmail.com>
2023-07-27 19:34:15 +0900

    Finish layout and routes

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "GET /signup" do
+    it "returns http success" do
+      get signup_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'Sign up | Ruby on Rails Tutorial Sample Appが含まれること' do
+      get signup_path
+      expect(response.body).to include full_title('Sign up')
+    end
+  end
+end
