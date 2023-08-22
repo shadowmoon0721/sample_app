1226a3ed1030d62f0ad7fad1f79482071d6bbad0
shadowmoon0721 <shadowmoon0721@gmail.com>
2023-08-23 03:19:09 +0900

    Implement basic login

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe "Sessions", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'DELETE /logout' do
+    it 'ログアウトできること' do
+      user = FactoryBot.create(:user)
+      post login_path, params: { session: { email: user.email, password: user.password } }
+      expect(is_logged_in?).to be_truthy
+
+      delete logout_path
+      expect(is_logged_in?).to_not be_truthy
+    end
+  end
 end
